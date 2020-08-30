011654f0f2e306b928d0667209c27cbcc207de17
uma_s <uma_s@DESKTOP-SKA6I02>
2020-08-30 16:17:12 -0700

    Added Third line

--- JavaToGitHub/src/Prog1.java
+++ JavaToGitHub/src/Prog1.java
@@ -4,6 +4,7 @@ public class Prog1 {
 	public static void main(String[] args) {
 		System.out.println("Try the send Java code to Git");
 		System.out.println("Second Line");
+		System.out.println("Third Line");
 	}
 
 }
